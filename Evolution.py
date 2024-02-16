6165488db7f4d9c3a7bbc1d1bcc470a62ed87f69
jmiguelmangas <jmmangas@gmail.com>
2024-02-16 16:56:43 +0100

    Two graphics, but we need to transform goals from 2021-2022 and rename the Dribbling succesful column in 2022-2023

--- Evolution.py
+++ Evolution.py
@@ -11,10 +11,10 @@ kylian_mbappe_21_22 = football_european_stats_21_22[football_european_stats_21_2
 vinicius_junior_21_22 = football_european_stats_21_22[football_european_stats_21_22["Player"]=="Vinicius Junior"]
 erling_haaland_21_22 = football_european_stats_21_22[football_european_stats_21_22["Player"]=="Erling Haaland"]
 
-jude_bellingham_22_23 = football_european_stats_21_22[football_european_stats_21_22["Player"]=="Jude Bellingham"]
-kylian_mbappe_22_23 = football_european_stats_21_22[football_european_stats_21_22["Player"]=="Kylian Mbappe"]
-vinicius_junior_22_23 = football_european_stats_21_22[football_european_stats_21_22["Player"]=="Vinicius Junior"]
-erling_haaland_22_23 = football_european_stats_21_22[football_european_stats_21_22["Player"]=="Erling Haaland"]
+jude_bellingham_22_23 = football_european_stats_22_23[football_european_stats_22_23["Player"]=="Jude Bellingham"]
+kylian_mbappe_22_23 = football_european_stats_22_23[football_european_stats_22_23["Player"]=="Kylian Mbappe"]
+vinicius_junior_22_23 = football_european_stats_22_23[football_european_stats_22_23["Player"]=="Vinicius Junior"]
+erling_haaland_22_23 = football_european_stats_22_23[football_european_stats_22_23["Player"]=="Erling Haaland"]
 
 best_in_europe_21_22 = pd.concat([jude_bellingham_21_22,kylian_mbappe_21_22,vinicius_junior_21_22,erling_haaland_21_22])
 best_in_europe_22_23 = pd.concat([jude_bellingham_22_23,kylian_mbappe_22_23,vinicius_junior_22_23,erling_haaland_22_23])
@@ -28,15 +28,17 @@ print(best_in_europe_21_22)
 print(best_in_europe_22_23)
 
 goals_shoot_21_22 = best_in_europe_21_22[["Player","Shots","Goals","SoT","PasAss","GCA","DriSucc"]]
+goals_shoot_22_23 = best_in_europe_22_23[["Player","Shots","Goals","SoT","PasAss","GCA","ToSuc"]]
+
+
+print(goals_shoot_21_22)
+print(goals_shoot_22_23)
 
 goals_shoot_21_22.set_index("Player",inplace=True)
+goals_shoot_22_23.set_index("Player",inplace=True)
 
 goals_shoot_21_22.plot(kind="barh")
 
-
-
-
-
-plt.title("2021-2022 Best Players in Europe Standings")
+goals_shoot_22_23.plot(kind="barh")
 
 plt.show()
